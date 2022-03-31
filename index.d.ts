77a933a95b14e4e72c7bde56b30458a7752e2107
jianchau <ajianchau@gmail.com>
2022-04-01 02:25:15 +0800

    fix export

--- index.d.ts
+++ index.d.ts
@@ -1,57 +1,58 @@
-import { Observer } from 'rxjs/internal/types';
-import { WebSocketSubject, WebSocketSubjectConfig } from 'rxjs/webSocket';
-import { WsClient } from './src/WSClient';
+import { Observer } from "rxjs/internal/types";
+import { WebSocketSubject, WebSocketSubjectConfig } from "rxjs/webSocket";
+import { WsClient, WsClientConnecter } from "./src/WSClient";
+export { WsClient, WsClientConnecter };
 /**
  * Ws remote interactive types.
  */
 export interface IStarWsInteractive {
-  /// Server name or Service name.
-  /// eg: 'pharmacy' or 'medservice'
-  userAgent: string;
-  /// The request id
-  requestId: string;
-  /// The api router.
-  bizCode?: string;
-  /// passed data
-  data: any;
-  [propName: string]: any;
+	/// Server name or Service name.
+	/// eg: 'pharmacy' or 'medservice'
+	userAgent: string;
+	/// The request id
+	requestId: string;
+	/// The api router.
+	bizCode?: string;
+	/// passed data
+	data: any;
+	[propName: string]: any;
 }
 
 // pass data of subject.subscribe
 export interface INextMsg {
-  cmd: number;
-  data: any;
+	cmd: number;
+	data: any;
 }
 
 // The ws response
 export interface IStarWsResponse extends IStarWsInteractive {
-  bizCode: string;
-  code: string; /// The status code
-  data: any; /// Payload
-  message: string; /// msg
-  requestId: string;
-  userAgent: string;
-  [propName: stiring]: any;
+	bizCode: string;
+	code: string; /// The status code
+	data: any; /// Payload
+	message: string; /// msg
+	requestId: string;
+	userAgent: string;
+	[propName: stiring]: any;
 }
 
 /// Connect params of the target wss.
 export interface StarWsClientConfig<T> extends WebSocketSubjectConfig<T> {
-  callback: Partial<Observer<T>> | ((value: T) => void); // accept msg from server
-  agent: number;
-  client: number;
-  enableLog?: boolean; // wheather log msg or not
+	callback: Partial<Observer<T>> | ((value: T) => void); // accept msg from server
+	agent: number;
+	client: number;
+	enableLog?: boolean; // wheather log msg or not
 }
 
 export interface StarWsClient {
-  /// 客户端配置
-  config: StarWsClientConfig<any>;
-  /// 客户端实例
-  instance: WsClient;
+	/// 客户端配置
+	config: StarWsClientConfig<any>;
+	/// 客户端实例
+	instance: WsClient;
 }
 
 export interface StarWsClientMap {
-  /// 客户端别名
-  [alias: string]: StarWsClient | any;
+	/// 客户端别名
+	[alias: string]: StarWsClient | any;
 }
 
 // // 请求类型
@@ -76,8 +77,8 @@ export interface StarWsClientMap {
 
 // Agent类型
 export enum StarAgentType {
-  WEB = 0,
-  APP = 1,
-  WINDOWS = 2,
-  Miniprogram = 3,
+	WEB = 0,
+	APP = 1,
+	WINDOWS = 2,
+	Miniprogram = 3,
 }
